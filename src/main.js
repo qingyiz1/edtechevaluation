fb74fe9b68e55f3a6cfa2aa2329df09af193151b
Qingyi Zhou <zhouqingyi1104@gmail.com>
2020-09-05 22:30:32 +1000

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,25 +1,21 @@
-import './tools/firebaseConfig';
 import Vue from 'vue'
 import App from './App.vue'
 import router from "./router"
 import {firestorePlugin } from 'vuefire'
-import axios from "axios";
-import firebase from "firebase";
 import './tools/bootstrap-vue';
 import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue';
+import {auth} from "@/tools/firebaseConfig";
 
-
-Vue.prototype.$axios = axios;
 Vue.use(firestorePlugin)
 Vue.use(BootstrapVue)
 Vue.use(BootstrapVueIcons)
 Vue.config.productionTip = false
 let app
 
-firebase.auth().onAuthStateChanged(async user => {
+auth.onAuthStateChanged(async user => {
   if (!app) {
     //wait to get user
-    user = await firebase.auth().currentUser;
+    user = await auth.currentUser;
 
     //start app
     app = new Vue({
