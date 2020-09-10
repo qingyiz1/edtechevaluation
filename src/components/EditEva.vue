30ff7dc35c8e0c1030a8ac3c305b4c6c26092faf
Qingyi Zhou <zhouqingyi1104@gmail.com>
2020-09-10 20:07:11 +1000

    editEvaluation UI update

--- src/components/EditEva.vue
+++ src/components/EditEva.vue
@@ -1,26 +1,43 @@
 <template>
   <div>
     <b-card no-body>
-      <b-tabs card>
-        <b-tab v-for="(section,indexS) in sections" :key="indexS" :title="section.name">
-          <b-card-group  deck>
-            <b-card v-for="(question,indexQ) in section.question" :key="indexQ" :header="question.questionName">
+      <b-tabs v-model="tabIndex" card>
+        <b-tab class="row" v-for="(section,indexS) in sections" :key="indexS" :title="section.name">
+          <b-card-group style="margin-bottom: 20px" v-for="(question,indexQ) in section.question" :key="indexQ" deck>
+            <b-card border-variant="secondary" :header="question.questionName">
+              <b-row no-gutters>
+              <b-col md="6">
               <b-list-group>
-                <b-list-group-item :class="{active: question.selected === 0}" button @click="select(indexS,indexQ,0)">1</b-list-group-item>
-                <b-list-group-item :class="{active: question.selected === 1}" button @click="select(indexS,indexQ,1)">2</b-list-group-item>
-                <b-list-group-item :class="{active: question.selected === 2}" button @click="select(indexS,indexQ,2)">3</b-list-group-item>
-                <b-list-group-item :class="{active: question.selected === 3}" button @click="select(indexS,indexQ,3)">4</b-list-group-item>
-                <b-list-group-item :class="{active: question.selected === 4}" button @click="select(indexS,indexQ,4)">5</b-list-group-item>
+                <b-list-group-item :class="{active: question.selected === 0}" button @click="select(indexS,indexQ,0)">Not Applicable</b-list-group-item>
+                <b-list-group-item :class="{active: question.selected === 1}" button @click="select(indexS,indexQ,1)">Below Basic</b-list-group-item>
+                <b-list-group-item :class="{active: question.selected === 2}" button @click="select(indexS,indexQ,2)">Basic</b-list-group-item>
+                <b-list-group-item :class="{active: question.selected === 3}" button @click="select(indexS,indexQ,3)">Adequate</b-list-group-item>
+                <b-list-group-item :class="{active: question.selected === 4}" button @click="select(indexS,indexQ,4)">Exceptional</b-list-group-item>
               </b-list-group>
+              </b-col>
 
-              <p class="card-text mt-2">
-                {{question.descriptions[question.selected]}}
-              </p>
+                <b-card class="w-100" border-variant="secondary" header="Description">
+                <b-card-text>
+                  {{question.descriptions[question.selected]}}
+                </b-card-text>
+                <p style="font-weight: bold">Comment:</p>
+                <textarea cols="6" v-model="question.comment" type="text" rows="2" class="form-control input-lg" name="comment" placeholder="Comment"></textarea>
+
+                </b-card>
+              </b-row>
             </b-card>
           </b-card-group>
         </b-tab>
       </b-tabs>
     </b-card>
+
+    <!-- Control buttons-->
+    <div class="text-center">
+      <b-button-group class="mt-2">
+        <b-button @click="tabIndex--">Previous</b-button>
+        <b-button @click="tabIndex++">Next</b-button>
+      </b-button-group>
+    </div>
   </div>
 </template>
 
@@ -34,12 +51,14 @@ export default {
   name: "EditEva",
   data() {
     return {
+      tabIndex: 1,
       sections: [],
       questions: {
         selected:0,
         name:"",
         description:"",
-        answer:""
+        answer:"",
+        comment:"",
       },
       report: "",
     };
