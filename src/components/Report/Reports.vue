7ab5c9d66cdb4846829bf2b826da37fc091fd9b2
Qingyi Zhou <zhouqingyi1104@gmail.com>
2020-09-24 01:31:29 +1000

    update report list

--- src/components/Report/Reports.vue
+++ src/components/Report/Reports.vue
@@ -1,84 +1,22 @@
 <template>
-<<<<<<< HEAD
-  <div class="list-container">
-    <b-row class="funtional-container">
-      <b-input-group size="sm" class="list-search">
-        <b-form-input type="search" placeholder="Search"></b-form-input>
-        <b-input-group-append is-text>
-          <b-icon icon="search"></b-icon>
-        </b-input-group-append>
-      </b-input-group>
-    </b-row>
-    <b-row class="list list-header" align-content="center">
-      <b-col cols="1">Author</b-col>
-      <b-col cols="2">Report Name</b-col>
-      <b-col cols="1">Evaluation</b-col>
-      <b-col cols="2">Created Time</b-col>
-      <b-col cols="2">Edited Time</b-col>
-      <b-col cols="1">Finish</b-col>
-      <b-col cols="1">Send</b-col>
-      <b-col cols="1">Download</b-col>
-      <b-col cols="1">Action</b-col>
-    </b-row>
-    <b-row
-      v-for="rep in reportList"
-      v-bind:key="rep.id"
-      class="list list-content"
-      align-content="center"
-      align-v="center"
-    >
-      <b-col cols="1">{{rep.author}}</b-col>
-      <b-col
-        cols="2"
-        @click="displayRep(rep.id)"
-        to="/framework"
-        class="list-content-display"
-      >{{rep.name}}</b-col>
-      <b-col cols="1">{{rep.evaluationId}}</b-col>
-      <b-col cols="2">{{rep.dateCreated.toDate().toLocaleString('en-US')}}</b-col>
-      <b-col cols="2">{{rep.dateEdited.toDate().toLocaleString('en-US')}}</b-col>
-      <b-col cols="1">
-        <b-icon
-          v-if="rep.isCompleted === true"
-          variant="success"
-          icon="check-circle-fill"
-          size="2.5rem"
-        ></b-icon>
-      </b-col>
-      <b-col cols="1">
-        <b-button
-          variant="info"
-          @click="openSendWindow(rep.id)"
-          class="list-inline-btn-sm"
-          :disabled="rep.isCompleted != true"
-        >Send</b-button>
-      </b-col>
-      <b-col cols="1">
-        <b-button
-          variant="info"
-          @click="downloadReport(rep.id)"
-          class="list-inline-btn-sm"
-          :disabled="rep.isCompleted != true"
-        >Download</b-button>
-=======
   <div>
     <b-modal
-          id="delete"
-          button-size="md"
-          centered
-          header-bg-variant="warning"
-          ok-title="Confirm"
-          ok-variant="danger"
-          size="md"
-          title="Are you sure?"
-          body-text-variant="danger"
-          @ok="deleteReport(repID)">
-         <div class="d-block text-center">
-           <h4>This action will delete the evaluation permanently!</h4>
-         </div>
-      </b-modal>
+        id="delete"
+        button-size="md"
+        centered
+        header-bg-variant="warning"
+        ok-title="Confirm"
+        ok-variant="danger"
+        size="md"
+        title="Are you sure?"
+        body-text-variant="danger"
+        @ok="deleteReport(repID)">
+      <div class="d-block text-center">
+        <h4>This action will delete the evaluation permanently!</h4>
+      </div>
+    </b-modal>
     <div class="list-container">
-      <b-row no-gutters class="funtional-container">
+      <b-row no-gutters class="functional-container">
         <b-input-group size="sm" class="list-search">
           <b-form-input type="search" placeholder="Search"></b-form-input>
           <b-input-group-append is-text>
@@ -91,56 +29,58 @@
         <b-col cols="2">Report Name</b-col>
         <b-col cols="2">Created Time</b-col>
         <b-col cols="2">Edited Time</b-col>
-        <b-col cols="1">Finish</b-col>
+        <b-col cols="1">Complete</b-col>
         <b-col cols="1">Send</b-col>
-        <b-col cols="2">Download</b-col>
-        <b-col cols="1">Action</b-col>
+        <b-col cols="1">Download</b-col>
+        <b-col cols="2">Action</b-col>
       </b-row>
-      <b-row 
-      no-gutters
-      v-for="rep in reportList" v-bind:key="rep.id" 
-      class="list list-content" 
-      align-content="center" 
-      align-v="center">
-        <b-col cols="1">{{rep.recommendationAuthor}}</b-col>
+      <b-row
+          no-gutters
+          v-for="rep in reportList" v-bind:key="rep.id"
+          class="list list-content"
+          align-content="center"
+          align-v="center">
+        <b-col cols="1">{{rep.author}}</b-col>
         <b-col cols="2" @click="displayRep(rep.id)" class="list-content-display">{{rep.name}}</b-col>
         <b-col cols="2">{{rep.dateCreated.toDate().toLocaleString('en-US')}}</b-col>
         <b-col cols="2">{{rep.dateEdited.toDate().toLocaleString('en-US')}}</b-col>
-        <b-col cols="1" style="padding-left:3.5rem">
+        <b-col cols="1">
           <b-form-checkbox
-          v-model="rep.isCompleted"
-          name="check-button"
-          size="lg"
-          switch
-          @change="toggleIsComplete(rep)">
+              v-model="rep.isCompleted"
+              name="check-button"
+              size="lg"
+              switch
+              @change="toggleIsComplete(rep)">
           </b-form-checkbox>
         </b-col>
         <b-col cols="1">
-          <b-button 
-          :disabled="!rep.isCompleted"
-          @click="openSendWindow(rep.id)"
-          class="list-inline-btn-sm">Send</b-button>
+          <b-button
+              :disabled="!rep.isCompleted"
+              @click="openSendWindow(rep.id)"
+              size="sm"
+              class="list-inline-btn-sm">Send</b-button>
         </b-col>
-        
-        <b-col cols="2">
-          <b-button 
-          :disabled="!rep.isCompleted"
-          @click="downloadReport(rep.id)"
-          class="list-inline-btn-sm">Download</b-button>
-        </b-col>
+
         <b-col cols="1">
-          <b-button 
-          variant="link"
-          @click="displayRep(rep.id)">
-            <b-avatar 
-            variant="success" 
-            icon="pencil" size="2rem"></b-avatar>
+          <b-button
+              :disabled="!rep.isCompleted"
+              @click="downloadReport(rep.id)"
+              size="sm"
+              class="list-inline-btn-sm">Download</b-button>
+        </b-col>
+        <b-col cols="2">
+          <b-button
+              variant="link"
+              @click="displayRep(rep.id)">
+            <b-avatar
+                variant="success"
+                icon="pencil" size="2rem"></b-avatar>
           </b-button>
-          <b-button 
-          variant="link"
-          v-b-modal.delete 
-          @click="setRepId(rep.id)"
-          style="padding:0"><b-avatar variant="danger" icon="trash" size="2rem"></b-avatar></b-button>
+          <b-button
+              variant="link"
+              v-b-modal.delete
+              @click="setRepId(rep.id)"
+              style="padding:0"><b-avatar variant="danger" icon="trash" size="2rem"></b-avatar></b-button>
         </b-col>
       </b-row>
     </div>
@@ -148,7 +88,7 @@
       <b-col>
         <div v-for="rep in reportList" v-bind:key="rep.id" class="report" >
           <b-card
-          :header="rep.name" 
+          :header="rep.name"
           class="text-left mt-3">
           <template v-slot:header>
             <b-row align-h="between" align-v="center">
@@ -179,32 +119,32 @@
                 {{rep.recommendationAuthor}}
             </b-card-text>
             <b-card-text>
-                <b-icon 
+                <b-icon
                 icon="calendar-date"
                 style="margin-right:10px"></b-icon>
                 {{rep.dateCreated.toDate().toLocaleString('en-US')}}
             </b-card-text>
             </b-col>
           <b-col cols="4">
-            <b-button 
+            <b-button
               :to="'/report_preview/'+rep.id"
-              size="sm" 
+              size="sm"
               style="margin-right: 1rem;margin-left:0.5rem"
               variant="primary"><b-icon icon="eye"></b-icon></b-button>
-            
-            <b-button 
+
+            <b-button
               size="sm"
               style="margin-right: 1rem;margin-left:0.5rem"
               variant="danger"
               @click="deleteReport(rep.id)"><b-icon icon="trash"></b-icon></b-button>
-            
-            <b-button 
+
+            <b-button
               size="sm"
               style="margin-right: 1rem;margin-left:0.5rem"
               variant="info"
               @click="downloadReport(rep.id)"><b-icon icon="download"></b-icon></b-button>
 
-              <b-button 
+              <b-button
               size="sm"
               style="margin-right: 1rem;margin-left:0.5rem"
               variant="info"
@@ -214,18 +154,8 @@
             </b-row>
           </b-card>
         </div>
->>>>>>> parent of eecf3c0... UI update
       </b-col>
-      <b-col cols="1">
-        <b-button variant="link" :to="'/report_preview/'+rep.id">
-          <b-avatar variant="success" icon="pencil" size="2rem"></b-avatar>
-        </b-button>
-        <b-button variant="link" @click="deleteReport(rep.id)">
-          <b-avatar variant="danger" icon="trash" size="2rem"></b-avatar>
-        </b-button>
-      </b-col>
-    </b-row>
-
+    </b-row> -->
     <el-dialog :visible.sync="sendWindowVisible" title="Send Report" width="360px">
       <el-form ref="form" :model="sendReport" label-width="80px">
         <el-form-item label="Email">
@@ -236,25 +166,25 @@
         </el-form-item>
       </el-form>
       <span slot="footer" class="dialog-footer">
-        <el-button @click="sendWindowVisible = false">Cancel</el-button>
-        <el-button type="primary" @click="sendEmail()">Confirm</el-button>
-      </span>
+      <el-button @click="sendWindowVisible = false">Cancel</el-button>
+      <el-button type="primary" @click="sendEmail()">Confirm</el-button>
+    </span>
     </el-dialog>
   </div>
 </template>
 
 <script>
-import { deleteDocument } from "@/tools/firebaseTool";
-import { reportCollection } from "@/tools/firebaseConfig";
-import { getDocument } from "@/tools/firebaseTool";
-import * as firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/firestore";
-import Docxtemplater from "docxtemplater";
-import PizZip from "pizzip";
-import JSZipUtils from "jszip-utils";
-import { saveAs } from "file-saver";
-import { Email } from "@/tools/smtp";
+import {deleteDocument, updateDocument} from "@/tools/firebaseTool";
+import {reportCollection} from "@/tools/firebaseConfig";
+import {getDocument} from "@/tools/firebaseTool";
+import * as firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+import Docxtemplater from 'docxtemplater'
+import PizZip from 'pizzip'
+import JSZipUtils from 'jszip-utils'
+import { saveAs } from 'file-saver'
+import {Email} from "@/tools/smtp"
 
 export default {
   name: "Reports",
@@ -263,50 +193,51 @@ export default {
       reportList: [],
       repInfoSend: {},
       sendWindowVisible: false,
-      sendReport: {
-        email: "",
-        message: "",
+      sendReport:{
+        email:"",
+        message:"",
       },
-      Id: "",
-      sections: [],
+      Id:"",
+      repID:""
     };
   },
   methods: {
-    async displayRep(id) {
-      await this.$router.push({ path: "/report_view_online/" + id });
+    deleteReport(repId){
+      deleteDocument("report",repId)
+      firebase.storage().ref().child('Report/'+repId+ '.docx').delete()
     },
-    deleteReport(repId) {
-      deleteDocument("report", repId);
-      firebase
-        .storage()
-        .ref()
-        .child("Report/" + repId + ".docx")
-        .delete();
+    toggleIsComplete(rep) {
+      updateDocument("report", rep.id, {"isCompleted": rep.isCompleted})
+    },
+    displayRep: function (repID) {
+      this.$router.push("/report_preview/" + repID)
     },
 
-    async downloadReport(repId) {
-      const repInfo = await getDocument("report", repId);
-      JSZipUtils.getBinaryContent("/ReportTemplate.docx", function (
-        error,
-        content
-      ) {
+    setRepId(id){
+      this.repID = id;
+    },
+
+    async downloadReport(repId){
+      const repInfo = await getDocument("report",repId)
+      await JSZipUtils.getBinaryContent('/Template.docx', function(error, content) {
+
         if (error) {
-          throw error;
+          throw error
         }
 
-        const zip = new PizZip(content);
-        const doc = new Docxtemplater().loadZip(zip);
+        const zip = new PizZip(content)
+        const doc = new Docxtemplater().loadZip(zip)
 
         doc.setData({
           repName: repInfo.name,
-          dateCreated: repInfo.dateCreated.toDate().toLocaleString("en-US"),
-          dateEdited: repInfo.dateEdited.toDate().toLocaleString("en-US"),
+          dateCreated: repInfo.dateCreated.toDate().toLocaleString('en-US'),
+          dateEdited: repInfo.dateEdited.toDate().toLocaleString('en-US'),
           repAuthor: repInfo.author,
           isCompleted: repInfo.isCompleted,
           content: repInfo.content.path,
           recommendation: repInfo.recommendation,
           recommendationAuthor: repInfo.recommendationAuthor,
-        });
+        })
         try {
           doc.render();
         } catch (error) {
@@ -314,64 +245,60 @@ export default {
             message: error.message,
             name: error.name,
             stack: error.stack,
-            properties: error.properties,
+            properties: error.properties
           };
           console.log(JSON.stringify({ error: e }));
           throw error;
         }
 
         const downloadReport = doc.getZip().generate({
-          type: "blob",
-          mimeType:
-            "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-        });
-        saveAs(downloadReport, repInfo.name + ".docx");
+          type: 'blob',
+          mimeType: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+        })
+        saveAs(downloadReport, repInfo.name + '.docx')
 
-        var storageRef = firebase.storage().ref();
-        var repRef = storageRef.child("Report/" + repId + ".docx");
+        let storageRef = firebase.storage().ref()
+        let repRef = storageRef.child('Report/'+repId+ '.docx')
 
-        var file = downloadReport;
-        repRef.put(file);
+        repRef.put(downloadReport)
       });
     },
-    async openSendWindow(repId) {
+    async openSendWindow(repId){
       this.sendWindowVisible = true;
-      global.repInfoSend = await getDocument("report", repId);
-      global.Id = repId;
+      global.repInfoSend = await getDocument("report",repId)
+      global.Id = repId
+
     },
     async sendEmail() {
-      var storageRef = firebase.storage().ref();
-      const url = await storageRef
-        .child("Report/" + global.Id + ".docx")
-        .getDownloadURL();
+      let storageRef = firebase.storage().ref()
+      const url = await storageRef.child('Report/'+global.Id+ '.docx').getDownloadURL()
 
       Email.send({
         Host: "smtp.gmail.com",
-        Username: "edtech.echidna@gmail.com",
-        Password: "edtechechidna",
-        To: this.sendReport.email,
-        From: "edtech.echidna@gmail.com",
-        Subject: "Report-" + global.repInfoSend.name,
-        Body: this.sendReport.message,
-        Attachments: [
+        Username : "edtech.echidna@gmail.com",
+        Password : "edtechechidna",
+        To : this.sendReport.email,
+        From : "edtech.echidna@gmail.com",
+        Subject : "Report-" + global.repInfoSend.name,
+        Body :this.sendReport.message,
+        Attachments : [
           {
-            name: global.repInfoSend.name + ".docx",
-            path: url,
-          },
-        ],
-      }).then(
-        (this.sendWindowVisible = false),
-        window.alert("Report has been sent to " + this.sendReport.email),
-        (this.sendReport.email = ""),
-        (this.sendReport.message = ""),
-        (global.repInfoSend = {}),
-        (global.Id = "")
-      );
-    },
+            name : global.repInfoSend.name + ".docx",
+            path : url
+          }]
+      }).then(()=>{
+          this.sendWindowVisible = false
+          window.alert("Report has been sent to "+ this.sendReport.email)
+          this.sendReport.email=""
+          this.sendReport.message=""
+          global.repInfoSend = {}
+          global.Id = ""}
+      )
+    }
   },
-  firestore: {
-    reportList: reportCollection,
-  },
+  firestore:{
+    reportList:reportCollection
+  }
 };
 </script>
 
@@ -380,4 +307,5 @@ export default {
 <style scoped>
 @import "../../css/general.css";
 @import "../../css/list.css";
+
 </style>
